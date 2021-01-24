21ab34f942989c9401a23a2c71a18b396c683b87
Francesco Luppi <francescoluppi@aol.com>
2021-01-24 18:43:11 +0100

    Adding CORS to all of the api folders

--- pages/api/v1/consultori.js
+++ pages/api/v1/consultori.js
@@ -1,8 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from 'cors'
+import initMiddleware from '../../../lib/init-middleware'
 
+//JSON File Path
 import consultori from '../../data/JSON/consultori.json'
 
-export default (req, res) => {
-  res.statusCode = 200
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res)
+
+  // Rest of the API logic
   res.json(consultori)
 }
