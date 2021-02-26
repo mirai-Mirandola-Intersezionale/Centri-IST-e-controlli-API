eccc858e5ac38e4650bf0c92954eb87b51d88760
Francesco Luppi <francescoluppi@aol.com>
2021-02-26 17:03:38 +0100

    DEL: tailwind e altro

--- pages/api/v1/associazioni.js
+++ pages/api/v1/associazioni.js
@@ -9,7 +9,7 @@ const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ['GET'],
   })
 )
 
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>/api.mirai - API libere per tutt*</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>/api.<b>mirai</b></h1>
-      
     </div>
   )
 }
